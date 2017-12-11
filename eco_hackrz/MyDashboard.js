b963957704653a3098fe41b3989f24af3313ff51
mailaw <mlaw534@gmail.com>
2017-12-10 22:37:03 -0500

    css styling

--- eco_hackrz/MyDashboard.js
+++ eco_hackrz/MyDashboard.js
@@ -38,3 +38,55 @@ $(function () {
 
     });
 });
+
+//PULSE
+$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
+
+    Highcharts.chart('pulse-container', {
+        chart: {
+            zoomType: 'x'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 3
+                },
+                lineWidth: 0.5,
+                states: {
+                    hover: {
+                        lineWidth: 0
+                    }
+                },
+                threshold: null
+            }
+        },
+
+        series: [{
+            type: 'area',
+            name: 'USD to EUR',
+            data: data
+        }]
+    });
+});
+
+$('#progress-bar')
+  .progress('set progress', 50)
+;
